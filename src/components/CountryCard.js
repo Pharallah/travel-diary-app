9b65764812200dee4c50686321daf0939ed44690
Harry San Juan <harrycsanjuan@gmail.com>
2024-03-12 01:03:39 -0400

    add favorite feature on countrycard

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function CountryCard({ country }) {
-  
+  const [favorite, setFavorite] = useState()
+
+  useEffect(() => {
+    if(country.favorite === true) {
+        setFavorite(true)
+    } else {
+        setFavorite(false)
+    }
+  }, [favorite])
 
     return (
     <li className="card">
       <h4>{country.country}</h4>
       <img src={country.flagImage} alt={country.country} />
       <p>Capital: {country.capital}</p>
+      {favorite ? <button>Unfavorite ♡</button> : <button >Favorite ♥️</button>}
     </li>
   )
 }
